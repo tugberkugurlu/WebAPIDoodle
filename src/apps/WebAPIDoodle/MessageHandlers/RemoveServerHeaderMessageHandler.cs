3b46ce64cd22b72eab36056827231bcebd00cfff
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-06-19 15:56:25 +0300

    added RemoveServerHeaderMessageHandler

---
+++ src/apps/WebAPIDoodle/MessageHandlers/RemoveServerHeaderMessageHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebAPIDoodle.MessageHandlers {
+
+    public class RemoveServerHeaderMessageHandler : DelegatingHandler {
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken) {
+
+            return base.SendAsync(request, cancellationToken).ContinueWith(task => {
+
+                var response = task.Result;
+
+                var httpContext = response.RequestMessage.Properties["MS_HttpContext"] as HttpContextWrapper;
+                if (httpContext != null)
+                    httpContext.Response.Headers.Remove("Server");
+
+                return response;
+            });
+        }
+    }
+}
