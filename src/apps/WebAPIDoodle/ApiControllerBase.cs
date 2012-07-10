12da7240fa1c1f7a5fb8e541daaf8e746202e9e0
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-10 19:26:20 +0300

    added ApiControllerBase

---
+++ src/apps/WebAPIDoodle/ApiControllerBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace WebAPIDoodle {
+
+    public abstract class ApiControllerBase : ApiController, IApiControllerBase {
+
+        public virtual void OnActionExecuting(HttpControllerContext controllerContext) { }
+        public virtual void OnActionExecuted(HttpResponseMessage response) { }
+
+        protected override void Initialize(HttpControllerContext controllerContext) {
+            
+            base.Initialize(controllerContext);
+            OnActionExecuting(controllerContext);
+        }
+
+        public override Task<HttpResponseMessage> ExecuteAsync(HttpControllerContext controllerContext, CancellationToken cancellationToken) {
+            
+            //TODO: Handle this better. E.g: What happens when an exception is thrown?
+            return base.ExecuteAsync(controllerContext, cancellationToken).ContinueWith(task => {
+
+                var response = task.Result;
+                OnActionExecuted(response);
+                return response;
+            });
+        }
+    }
+}
---
+++ src/apps/WebAPIDoodle/IApiControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http.Controllers;
+
+namespace WebAPIDoodle {
+
+    public interface IApiControllerBase {
+
+        void OnActionExecuting(HttpControllerContext controllerContext);
+        void OnActionExecuted(HttpResponseMessage response);
+    }
+}
