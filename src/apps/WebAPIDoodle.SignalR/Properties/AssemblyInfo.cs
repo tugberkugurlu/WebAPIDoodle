618a0edfb30b08514bf58cf39dfc244b75e6bbbe
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-12 12:51:44 +0300

    made some changes on WebAPIDoodle.SignalR.nuspec

--- src/apps/WebAPIDoodle.SignalR/Properties/AssemblyInfo.cs
+++ src/apps/WebAPIDoodle.SignalR/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("WebAPIDoodle")]
-[assembly: AssemblyDescription("WebAPIDoodle is an ASP.NET Web API library which contains several useful Extensions, Filters, Message Handlers and so on and so forth.")]
+[assembly: AssemblyTitle("WebAPIDoodle.SignalR")]
+[assembly: AssemblyDescription("WebAPIDoodle.SignalR is an ASP.NET Web API library which contains some SignalR integrated ASP.NET Web API stuff.")]
 
 [assembly: AssemblyVersion("0.1.1.0")]
 [assembly: AssemblyFileVersion("0.1.1.0")]
