6815dc5c47acc01a9e306918fa00b4f6816aa9bd
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-16 17:30:14 +0300

    added RequireHttpsMessageHandler

---
+++ src/apps/WebAPIDoodle/MessageHandlers/RequireHttpsMessageHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebAPIDoodle.Messages;
+
+namespace WebAPIDoodle.MessageHandlers {
+
+    public class RequireHttpsMessageHandler : DelegatingHandler {
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+
+            if (request.RequestUri.Scheme != Uri.UriSchemeHttps) {
+
+                return CachedSSLRequiredResponseMessageItem.SSLRequiredForbiddenResponseMessage;
+            }
+   
+            return base.SendAsync(request, cancellationToken);
+        }
+
+        private static class CachedSSLRequiredResponseMessageItem {
+
+            public static Task<HttpResponseMessage> SSLRequiredForbiddenResponseMessage = GetSSLRequiredForbiddenResponseMessage();
+
+            private static Task<HttpResponseMessage> GetSSLRequiredForbiddenResponseMessage() { 
+
+                return TaskHelpers.FromResult<HttpResponseMessage>(
+                    new HttpResponseMessageWithStringContent("SSL required", HttpStatusCode.Forbidden));
+            }
+        }
+    }
+}
