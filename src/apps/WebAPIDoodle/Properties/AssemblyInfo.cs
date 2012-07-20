74b7110a7f86dd9174d3d31985b825f69c90f711
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-20 18:40:31 +0300

    0.1.1 => 0.1.2

--- src/apps/WebAPIDoodle/Properties/AssemblyInfo.cs
+++ src/apps/WebAPIDoodle/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("WebAPIDoodle")]
 [assembly: AssemblyDescription("WebAPIDoodle is an ASP.NET Web API library which contains several useful Extensions, Filters, Message Handlers and so on and so forth.")]
 
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
-[assembly: AssemblyInformationalVersion("0.1.1")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
+[assembly: AssemblyInformationalVersion("0.1.2")]
