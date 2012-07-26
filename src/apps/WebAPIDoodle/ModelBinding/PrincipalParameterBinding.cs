c9cf77034c400920d66654439ec02ed2773f34e9
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-26 17:23:50 +0300

    added PrincipalParameterBinding

---
+++ src/apps/WebAPIDoodle/ModelBinding/PrincipalParameterBinding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Metadata;
+
+namespace WebAPIDoodle.ModelBinding {
+
+    public class PrincipalParameterBinding : HttpParameterBinding {
+
+        public PrincipalParameterBinding(HttpParameterDescriptor descriptor) 
+            : base(descriptor) { 
+        }
+
+        public override Task ExecuteBindingAsync(ModelMetadataProvider metadataProvider, HttpActionContext actionContext, CancellationToken cancellationToken) {
+
+            string name = Descriptor.ParameterName;
+            IPrincipal principal = Thread.CurrentPrincipal;
+            actionContext.ActionArguments.Add(name, principal);
+
+            return TaskHelpers.Completed();
+        }
+    }
+}
