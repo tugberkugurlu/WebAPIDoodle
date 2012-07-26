de1a133466db4eb15cc69cfa05097552dd2e0535
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-26 19:56:25 +0300

    corrected the name of the formatting proj

--- src/apps/WebAPIDoodle.Formatting/Properties/AssemblyInfo.cs
+++ src/apps/WebAPIDoodle.Formatting/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyTitle("WebAPIDoodle")]
+[assembly: AssemblyTitle("WebAPIDoodle.Formatting")]
 [assembly: AssemblyDescription("WebAPIDoodle.Formatting is a library which contains several formatters for System.Net.Http.Formatting.")]
