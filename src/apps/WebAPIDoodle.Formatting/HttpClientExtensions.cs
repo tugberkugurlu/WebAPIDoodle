371721913efcae054d6d77ac6fb62df939aa45a9
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-28 18:36:34 +0300

    added HttpClientExtensions

---
+++ src/apps/WebAPIDoodle.Formatting/HttpClientExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPIDoodle.Formatting {
+
+    public static class HttpClientExtensions {
+
+        public static Task<HttpResponseMessage> PostAsCsvAsync<T>(this HttpClient client, string requestUri, T value) {
+
+            return client.PostAsCsvAsync(requestUri, value, CancellationToken.None);
+        }
+
+        public static Task<HttpResponseMessage> PostAsCsvAsync<T>(this HttpClient client, string requestUri, T value, CancellationToken cancellationToken) {
+
+            return client.PostAsync(requestUri, value, new CSVMediaTypeFormatter(), cancellationToken);
+        }
+    }
+}
