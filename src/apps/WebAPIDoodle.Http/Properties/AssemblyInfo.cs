958a1fd06ae2266c07fad15e0ceb0095f553e201
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-28 18:27:20 +0300

    added a new project: WebAPIDoodle.Http and seperated out the Http specific bits so that they can be used outside of the ASP.NET Web API. Released the preview2 of the 0.2.2

---
+++ src/apps/WebAPIDoodle.Http/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("WebAPIDoodle.Http")]
+[assembly: AssemblyDescription("WebAPIDoodle.Http is a library which provides more features over System.Net.Http")]
