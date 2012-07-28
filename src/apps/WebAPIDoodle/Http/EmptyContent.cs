5f8ffecc2d1d2f22124f2484b1a180fab85712bc
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2012-07-28 17:44:50 +0300

    preview release and added EmptyContent

---
+++ src/apps/WebAPIDoodle/Http/EmptyContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace WebAPIDoodle.Http {
+
+    public class EmptyContent : StringContent {
+
+        public EmptyContent() : base(string.Empty) { }
+    }
+}
