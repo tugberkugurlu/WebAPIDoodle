8f68797b4fc572febffe32fe80b062d5744e244b
tugberkugurlu <tugberkugurlu@gmail.com>
2012-08-17 18:46:50 -0700

    webapi RTM update and version 1.0.0 release

--- src/apps/Common/CommonAssemblyInfo.cs
+++ src/apps/Common/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("0.2.2.0")]
-[assembly: AssemblyFileVersion("0.2.2.0")]
-[assembly: AssemblyInformationalVersion("0.2.3-pre01")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
