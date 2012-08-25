a220cffe3aee6b8bba39a6e51d113934041e5aa2
tugberkugurlu <tugberkugurlu@gmail.com>
2012-08-25 19:13:22 +0300

    added ApiKeyAuthenticationHandler and ApiKeyAuthenticationHandlerTest

---
+++ src/apps/WebAPIDoodle/Http/Handlers/ApiKeyAuthenticationHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebAPIDoodle.Http.Handlers {
+
+    public abstract class ApiKeyAuthenticationHandler : DelegatingHandler {
+
+        private readonly string _apiKeyQueryParameter;
+
+        public ApiKeyAuthenticationHandler(string apiKeyQueryParameter) {
+
+            if (string.IsNullOrEmpty(apiKeyQueryParameter)) {
+
+                throw new ArgumentNullException("apiKeyQueryParameter");
+            }
+
+            _apiKeyQueryParameter = apiKeyQueryParameter;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+
+            var queryStringCollection = request.RequestUri.ParseQueryString();
+            if (queryStringCollection.AllKeys.Any(key => key.Equals(_apiKeyQueryParameter, StringComparison.OrdinalIgnoreCase))) {
+
+                var apiKey = queryStringCollection[_apiKeyQueryParameter];
+                IPrincipal principal;
+
+                try {
+
+                    //Authenticate the user now
+                    principal = AuthenticateUser(apiKey, request, cancellationToken);
+                }
+                catch (Exception e) {
+
+                    return TaskHelpers.FromError<HttpResponseMessage>(e);
+                }
+
+                //check if the user has been authenticated successfully
+                if (principal != null) {
+
+                    Thread.CurrentPrincipal = principal;
+                    return base.SendAsync(request, cancellationToken);
+                }
+            }
+
+            return TaskHelpers.FromResult(new HttpResponseMessage(HttpStatusCode.Unauthorized));
+        }
+
+        /// <summary>
+        /// The method which is responsable for authenticating the user based on the provided API Key and request.
+        /// </summary>
+        /// <param name="apiKey"></param>
+        /// <param name="request"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        protected abstract IPrincipal AuthenticateUser(string apiKey, HttpRequestMessage request, CancellationToken cancellationToken);
+    }
+}
---
+++ src/tests/WebAPIDoodle.Test/MessageHandlers/ApiKeyAuthenticationHandlerTest.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WebAPIDoodle.Http;
+using WebAPIDoodle.Http.Handlers;
+using Xunit;
+
+namespace WebAPIDoodle.Test.MessageHandlers {
+    
+    public class ApiKeyAuthenticationHandlerTest {
+
+        private const string FakeApiKey = "fb0ab544-232e-46cf-be06-ee3619e61a42";
+        private const string DesignatedApiKeyQueryStringParameter = "apiKey";
+        private const string BaseRequestUri = "http://localhost/";
+
+        [Fact, GCForce]
+        public Task ApiKeyAuthenticationHandler_ReturnsUnauthorizedIfApiKeyQueryStringParameterIsNotSupplied() {
+
+            //Arange
+            var request = new HttpRequestMessage(HttpMethod.Get, BaseRequestUri);
+            var customApiKeyAuthHandler = new CustomApiKeyAuthHandler(DesignatedApiKeyQueryStringParameter);
+
+            //Act
+            return TestHelper.InvokeMessageHandler(request, customApiKeyAuthHandler)
+
+                .ContinueWith(task => {
+
+                    //Assert
+                    Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+                    Assert.Equal(HttpStatusCode.Unauthorized, task.Result.StatusCode);
+                });
+        }
+
+        [Fact, GCForce]
+        public Task ApiKeyAuthenticationHandler_ReturnsUnauthorizedIfApiKeyQueryStringValueIsNotValid() {
+
+            //Arange
+            var requestUri = string.Format("{0}?{1}={2}", BaseRequestUri, DesignatedApiKeyQueryStringParameter, "invalid-api-key");
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            var customApiKeyAuthHandler = new CustomApiKeyAuthHandler(DesignatedApiKeyQueryStringParameter);
+
+            //Act
+            return TestHelper.InvokeMessageHandler(request, customApiKeyAuthHandler)
+
+                .ContinueWith(task => {
+
+                    //Assert
+                    Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+                    Assert.Equal(HttpStatusCode.Unauthorized, task.Result.StatusCode);
+                });
+        }
+
+        [Fact, GCForce]
+        public Task ApiKeyAuthenticationHandler_Returns200IfApiKeyQueryStringValueIsValid() {
+
+            //Arange
+            var requestUri = string.Format("{0}?{1}={2}", BaseRequestUri, DesignatedApiKeyQueryStringParameter, FakeApiKey);
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            var customApiKeyAuthHandler = new CustomApiKeyAuthHandler(DesignatedApiKeyQueryStringParameter);
+
+            //Act
+            return TestHelper.InvokeMessageHandler(request, customApiKeyAuthHandler)
+
+                .ContinueWith(task => {
+
+                    //Assert
+                    Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+                    Assert.Equal(HttpStatusCode.OK, task.Result.StatusCode);
+                });
+        }
+
+        [Fact, GCForce]
+        public Task ApiKeyAuthenticationHandler_SetsCurrentThreadPrincipalWhenApiKeyIsValid() {
+
+            //Arange
+            var requestUri = string.Format("{0}?{1}={2}", BaseRequestUri, DesignatedApiKeyQueryStringParameter, FakeApiKey);
+            var request = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            var customApiKeyAuthHandler = new CustomApiKeyAuthHandler(DesignatedApiKeyQueryStringParameter);
+
+            //Act
+            return TestHelper.InvokeMessageHandler(request, customApiKeyAuthHandler)
+
+                .ContinueWith(task => {
+
+                    //Assert
+                    Assert.Equal(TaskStatus.RanToCompletion, task.Status);
+                    Assert.Equal(HttpStatusCode.OK, task.Result.StatusCode);
+                    Assert.IsType<GenericPrincipal>(Thread.CurrentPrincipal);
+                });
+        }
+
+        public class CustomApiKeyAuthHandler : ApiKeyAuthenticationHandler {
+
+            public CustomApiKeyAuthHandler(string apiKeyQueryParameter) : base(apiKeyQueryParameter) { }
+
+            protected override IPrincipal AuthenticateUser(string apiKey, HttpRequestMessage request, CancellationToken cancellationToken) {
+
+                if (apiKey.Equals(FakeApiKey, StringComparison.OrdinalIgnoreCase)) {
+
+                    var identity = new GenericIdentity("fakeuser");
+                    return new GenericPrincipal(identity, null);
+                }
+
+                return null;
+            }
+        }
+    }
+}
