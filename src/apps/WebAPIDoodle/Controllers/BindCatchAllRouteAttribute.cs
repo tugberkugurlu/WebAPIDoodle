63ba244ef3ad78b38c8c989b22af0221cca02406
tugberkugurlu <tugberkugurlu@gmail.com>
2012-08-31 23:52:53 +0300

    Added CatchAllRouteParameterBinding and BindCatchAllRouteAttribute. More info: http://www.tugberkugurlu.com/archive/asp-net-web-api-catch-all-route-parameter-binding

---
+++ src/apps/WebAPIDoodle/Controllers/BindCatchAllRouteAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace WebAPIDoodle.Controllers {
+
+    public class BindCatchAllRouteAttribute : ParameterBindingAttribute {
+
+        private readonly char _delimiter;
+
+        public BindCatchAllRouteAttribute(char delimiter) {
+
+            _delimiter = delimiter;
+        }
+
+        public override HttpParameterBinding GetBinding(HttpParameterDescriptor parameter) {
+
+            return new CatchAllRouteParameterBinding(parameter, _delimiter);
+        }
+    }
+}
---
+++ src/apps/WebAPIDoodle/Controllers/CatchAllRouteParameterBinding.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+
+namespace WebAPIDoodle.Controllers {
+
+    public class CatchAllRouteParameterBinding : HttpParameterBinding {
+
+        private readonly string _parameterName;
+        private readonly char _delimiter;
+
+        public CatchAllRouteParameterBinding(
+            HttpParameterDescriptor descriptor, char delimiter)
+            : base(descriptor) {
+
+            _parameterName = descriptor.ParameterName;
+            _delimiter = delimiter;
+        }
+
+        public override Task ExecuteBindingAsync(
+            System.Web.Http.Metadata.ModelMetadataProvider metadataProvider,
+            HttpActionContext actionContext,
+            CancellationToken cancellationToken) {
+
+            var routeValues = actionContext.ControllerContext.RouteData.Values;
+
+            if (routeValues[_parameterName] != null) {
+
+                string[] catchAllValues =
+                    routeValues[_parameterName].ToString().Split(_delimiter);
+
+                actionContext.ActionArguments.Add(_parameterName, catchAllValues);
+            }
+            else {
+
+                actionContext.ActionArguments.Add(_parameterName, new string[0]);
+            }
+
+            return TaskHelpers.Completed();
+        }
+    }
+}
