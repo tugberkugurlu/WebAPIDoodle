ada3ce4bcfe211e14649e40df952216e40259b32
tugberkugurlu <tugberkugurlu@gmail.com>
2012-09-03 19:17:38 +0300

    added OptionalRegExConstraint

---
+++ src/apps/WebAPIDoodle/Routes/OptionalRegExConstraint.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace WebAPIDoodle.Routes {
+
+    public class OptionalRegExConstraint : IHttpRouteConstraint {
+
+        private readonly string _regEx;
+
+        public OptionalRegExConstraint(string expression) {
+
+            if (string.IsNullOrEmpty(expression)) {
+
+                throw Error.ArgumentNull("expression");
+            }
+
+            _regEx = expression;
+        }
+
+        public bool Match(
+            HttpRequestMessage request, 
+            IHttpRoute route, 
+            string parameterName, 
+            IDictionary<string, object> values, 
+            HttpRouteDirection routeDirection) {
+
+            if (values[parameterName] != RouteParameter.Optional) {
+
+                object value;
+                values.TryGetValue(parameterName, out value);
+                string input = Convert.ToString(value, CultureInfo.InvariantCulture);
+
+                string pattern = "^(" + _regEx + ")$";
+                return Regex.IsMatch(input, pattern, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/tests/WebAPIDoodle.Test/Routes/OptionalRegExConstraintTest.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Http.Routing;
+using WebAPIDoodle.Routes;
+using Xunit;
+
+namespace WebAPIDoodle.Test.Routes {
+    
+    public class OptionalRegExConstraintTest {
+
+        [Fact]
+        public void OptionalRegExConstraint_ThrowsArgumentNullException_If_ExpressionParamIsNull() {
+        }
+
+        [Fact]
+        public void Match_ReturnsTrue_If_RouteValueIsRouteParamaterOptional() { 
+        }
+
+        [Fact]
+        public void Match_ReturnsTrue_If_RouteValueIsNotRouteParamaterOptional_And_RegExMatchesTheValue() {
+        }
+
+        [Fact]
+        public void Match_ReturnsFalse_If_RouteValueIsNotRouteParamaterOptional_And_RegExNotMatchesTheValue() {
+        }
+    }
+}
