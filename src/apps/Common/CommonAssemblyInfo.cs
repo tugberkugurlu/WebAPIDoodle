02c69d7a250e812c815c90fd6de37db0ef10906f
tugberkugurlu <tugberkugurlu@gmail.com>
2012-10-07 16:50:54 +0300

    1.2.2-pre01 => 1.2.2

--- src/apps/Common/CommonAssemblyInfo.cs
+++ src/apps/Common/CommonAssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("1.2.2.0")]
 [assembly: AssemblyFileVersion("1.2.2.0")]
-[assembly: AssemblyInformationalVersion("1.2.2-pre01")]
+[assembly: AssemblyInformationalVersion("1.2.2")]
