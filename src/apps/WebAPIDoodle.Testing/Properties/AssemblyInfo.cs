20368f55ea84592216547c96bd87ce34c798fed0
tugberkugurlu <tugberkugurlu@gmail.com>
2012-12-07 13:56:46 +0200

    added WebAPIDoodle.Testing proj

---
+++ src/apps/WebAPIDoodle.Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("WebAPIDoodle.Testing")]
+[assembly: AssemblyDescription("WebAPIDoodle.Testing is an ASP.NET Web API testing helper library which makes it easy to test ASP.NET Web API components.")]
