b072043b30467c2c42a313997259eee1479467fe
tugberkugurlu <tugberkugurlu@gmail.com>
2012-12-07 14:00:52 +0200

    added DelegatingHandlerExtensions inside the WebAPIDoodle.Testing proj

---
+++ src/apps/WebAPIDoodle.Testing/MessageHandlers/DelegatingHandlerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WSystem.Net.Http {
+
+    public static class DelegatingHandlerExtensions {
+
+        internal static Task<HttpResponseMessage> InvokeAsync(
+            this DelegatingHandler handler, HttpRequestMessage request, CancellationToken cancellationToken = default(CancellationToken)) {
+
+            handler.InnerHandler = new DummyHandler();
+            var invoker = new HttpMessageInvoker(handler);
+            return invoker.SendAsync(request, cancellationToken);
+        }
+
+        private class DummyHandler : HttpMessageHandler {
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+                return TaskHelpers.FromResult(response);
+            }
+        }
+    }
+}
