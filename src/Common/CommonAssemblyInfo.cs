898ec576ee30590b3d545ba57482aeaf970f0d65
Tugberk Ugurlu <tugberkugurlu@gmail.com>
2013-11-12 01:35:05 +0200

    bumped the version number to 3.0.0-pre01

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyConfiguration("")]
 
-[assembly: AssemblyVersion("2.0.1.0")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
-[assembly: AssemblyInformationalVersion("2.0.1")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0-pre01")]
